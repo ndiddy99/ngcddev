3203c1cfae3749c1d5aa661d0db76da7afbdad8d
Nathan Misner <nathan@thotpatrol.pw>
2019-12-04 22:11:02 -0500

    made animation conditional on moving, added mirroring and speed limiting

--- src_68k/player.asm
+++ src_68k/player.asm
@@ -1,10 +1,12 @@
 playerInit:
+	;init player variables
 	move.l #$00400000,player_xPos
 	move.l #$00500000,player_yPos
 	clr.l player_xSpeed
 	clr.l player_ySpeed
 	clr.w player_animTimer
 	clr.w player_tileIndex
+	;set up initial tilemap
 	move.w #SCB1+(33*64),LSPC_ADDR
 	move.w #1,LSPC_INCR
 	move.w #100,LSPC_DATA
@@ -33,33 +35,37 @@ playerSet:
 	rts
 
 PLAYER_ACCEL equ $8000
+PLAYER_MAX_SPEED equ $48000
+HORIZ_FLIP equ $1
 PlayerTiles:
 	dc.w 101,100,102,100,$FFFF
 playerMove:
-	move.w player_animTimer,d0
-	add.w #1,d0
-	cmp.w #7,d0
-	beq .animPlayer
-		move.w d0,player_animTimer
-		bra .dontAnim
-	.animPlayer:
-		clr.w player_animTimer
-		move.w player_tileIndex,d0
-		lea PlayerTiles,a0
-		move.w (a0,d0),d1
-		cmp.w #$FFFF,d1
-		bne .notEndList
-			move.w #0,d0
-			move.w (a0),d1
-	.notEndList:
-		add.w #2,d0 ;next word
-		move.w d0,player_tileIndex
-		;correct tile is now in d1
-		move.w #SCB1+(33*64),LSPC_ADDR
-		move.w #2,LSPC_INCR
-		move.w d1,LSPC_DATA
-		add.w #3,d1
-		move.w d1,LSPC_DATA
+	move.w player_xSpeed,d0
+	beq .dontAnim
+		move.w player_animTimer,d0
+		add.w #1,d0
+		cmp.w #7,d0
+		beq .animPlayer
+			move.w d0,player_animTimer
+			bra .dontAnim
+		.animPlayer:
+			clr.w player_animTimer
+			move.w player_tileIndex,d0
+			lea PlayerTiles,a0
+			move.w (a0,d0),d1
+			cmp.w #$FFFF,d1
+			bne .notEndList
+				move.w #0,d0
+				move.w (a0),d1
+		.notEndList:
+			add.w #2,d0 ;next word
+			move.w d0,player_tileIndex
+			;correct tile is now in d1
+			move.w #SCB1+(33*64),LSPC_ADDR
+			move.w #2,LSPC_INCR
+			move.w d1,LSPC_DATA
+			add.w #3,d1
+			move.w d1,LSPC_DATA
 	.dontAnim:
 	
 	move.b BIOS_P1CURRENT,d0
@@ -77,13 +83,43 @@ playerMove:
 			sub.l #PLAYER_ACCEL,player_xSpeed
 			bra .doneTests
 	.testDirs:
+		move.l player_xSpeed,d0
 		btst.b #JOY_LEFT,d1
 		beq .NoLeft
 			sub.l #PLAYER_ACCEL,player_xSpeed
+			cmp.l #-PLAYER_MAX_SPEED,d0
+			bpl .dontClampLSpeed
+				move.l #-PLAYER_MAX_SPEED,player_xSpeed
+			.dontClampLSpeed:
+			;mirror sprite so he's facing left
+			move.w #SCB1+(33*64)+1,LSPC_ADDR
+			clr.w LSPC_INCR
+			move.w LSPC_DATA,d0
+			or.w #HORIZ_FLIP,d0
+			move.w d0,LSPC_DATA
+			move.w #SCB1+(33*64)+3,LSPC_ADDR
+			move.w LSPC_DATA,d0
+			or.w #HORIZ_FLIP,d0
+			move.w d0,LSPC_DATA
+			
 		.NoLeft:
 		btst.b #JOY_RIGHT,d1
 		beq .NoRight
 			add.l #PLAYER_ACCEL,player_xSpeed
+			cmp.l #PLAYER_MAX_SPEED,d0
+			bmi .dontClampRSpeed
+				move.l #PLAYER_MAX_SPEED,player_xSpeed
+			.dontClampRSpeed:
+			;unmirror sprite just in case :)
+			move.w #SCB1+(33*64)+1,LSPC_ADDR
+			clr.w LSPC_INCR
+			move.w LSPC_DATA,d0
+			and.w #~HORIZ_FLIP,d0
+			move.w d0,LSPC_DATA	
+			move.w #SCB1+(33*64)+3,LSPC_ADDR
+			move.w LSPC_DATA,d0
+			and.w #~HORIZ_FLIP,d0
+			move.w d0,LSPC_DATA			
 		.NoRight:
 .doneTests:
 	move.l player_xPos,d0
